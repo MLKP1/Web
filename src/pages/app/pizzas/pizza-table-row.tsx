f54d1de40b1a9e8fa7ea53e59e8b25a1f8b59313
lucaslinyker <lucaslinyker@outlook.com>
2025-09-26 14:06:25 -0300

    feat: add pizza table row and skeleton components

---
+++ src/pages/app/pizzas/pizza-table-row.tsx
@@ -0,0 +1,201 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { ArrowRight, Loader2, Search, Trash } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'sonner'
+
+import { activePizza } from '@/api/active-pizza'
+import { disablePizza } from '@/api/disable-pizza'
+import { removePizza } from '@/api/remove-pizza'
+import { GetPizzasResponse } from '@/api/get-pizzas'
+import { PizzaStatus } from '@/components/pizza-status'
+import { PizzaType } from '@/components/pizza-type'
+import { PizzaSize } from '@/components/pizza-size'
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+import { PizzaDetails } from './pizza-details'
+
+export interface PizzaTableRowProps {
+  pizza: {
+    pizzaId: string
+    active: boolean
+    name: string
+    description: string
+    price: number
+    image: string
+    size: 'MEDIUM' | 'LARGE' | 'FAMILY'
+    type: 'SWEET' | 'SALTY'
+    slug: string
+  }
+}
+
+export function PizzaTableRow({ pizza }: PizzaTableRowProps) {
+  const [isPizzaDetailsOpen, setIsPizzaDetailsOpen] = useState(false)
+  const queryClient = useQueryClient()
+
+  function updatePizzaActiveOnCache(pizzaId: string, active: boolean) {
+    const pizzasListingCache = queryClient.getQueriesData<GetPizzasResponse>({
+      queryKey: ['pizzas'],
+    })
+
+    pizzasListingCache.forEach(([cacheKey, cached]) => {
+      if (!cached) {
+        return
+      }
+
+      queryClient.setQueryData<GetPizzasResponse>(cacheKey, {
+        ...cached,
+        pizzas: cached.pizzas.map((pizza) => {
+          if (pizza.pizzaId !== pizzaId) {
+            return pizza
+          }
+
+          return {
+            ...pizza,
+            active,
+          }
+        }),
+      })
+    })
+
+    toast.success('Status da pizza alterado com sucesso!')
+  }
+
+  const { mutateAsync: activatePizzaFn, isPending: isActivatingPizza } =
+    useMutation({
+      mutationFn: activePizza,
+      onSuccess: async (_, { pizzaId }) => {
+        updatePizzaActiveOnCache(pizzaId, true)
+      },
+    })
+
+  const { mutateAsync: disablePizzaFn, isPending: isDisablingPizza } =
+    useMutation({
+      mutationFn: disablePizza,
+      onSuccess: async (_, { pizzaId }) => {
+        updatePizzaActiveOnCache(pizzaId, false)
+      },
+    })
+
+  function removePizzaOnCache(pizzaId: string) {
+    const pizzasListingCache = queryClient.getQueriesData<GetPizzasResponse>({
+      queryKey: ['pizzas'],
+    })
+
+    pizzasListingCache.forEach(([cacheKey, cached]) => {
+      if (!cached) {
+        return
+      }
+
+      queryClient.setQueryData<GetPizzasResponse>(cacheKey, {
+        ...cached,
+        pizzas: cached.pizzas.filter((pizza) => pizza.pizzaId !== pizzaId)
+      })
+    })
+
+    toast.success('Pizza removida com sucesso!')
+  }
+
+  const { mutateAsync: removePizzaFn, isPending: isRemovingPizza } =
+    useMutation({
+      mutationFn: removePizza,
+      onSuccess: async (_, { pizzaId }) => {
+        removePizzaOnCache(pizzaId)
+      }
+    })
+
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog onOpenChange={setIsPizzaDetailsOpen} open={isPizzaDetailsOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes da pizza</span>
+            </Button>
+          </DialogTrigger>
+
+          <PizzaDetails open={isPizzaDetailsOpen} id={pizza.pizzaId} />
+        </Dialog>
+      </TableCell>
+
+      <TableCell className="font-mono text-xs font-medium">
+        {pizza.pizzaId}
+      </TableCell>
+
+      <TableCell className="font-medium">{pizza.name}</TableCell>
+
+      <TableCell>{pizza.description}</TableCell>
+
+      <TableCell>
+        <PizzaSize size={pizza.size} />
+      </TableCell>
+
+      <TableCell>
+        <PizzaType type={pizza.type} />
+      </TableCell>
+
+      <TableCell>
+        <PizzaStatus active={pizza.active} />
+      </TableCell>
+
+      <TableCell>
+        <span className="font-medium">
+          {(pizza.price / 100).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </span>
+      </TableCell>
+
+      <TableCell>
+        {pizza.active === false ? (
+          <Button
+            variant="outline"
+            size="xs"
+            disabled={isActivatingPizza}
+            onClick={() => activatePizzaFn({ pizzaId: pizza.pizzaId })}
+          >
+            Ativar
+            {isActivatingPizza ? (
+              <Loader2 className="ml-2 h-3 w-3 animate-spin" />
+            ) : (
+              <ArrowRight className="ml-2 h-3 w-3" />
+            )}
+          </Button>
+        ) : (
+          <Button
+            variant="outline"
+            size="xs"
+            disabled={isDisablingPizza}
+            onClick={() => disablePizzaFn({ pizzaId: pizza.pizzaId })}
+          >
+            Desativar
+            {isDisablingPizza ? (
+              <Loader2 className="ml-2 h-3 w-3 animate-spin" />
+            ) : (
+              <ArrowRight className="ml-2 h-3 w-3" />
+            )}
+          </Button>
+        )
+      }
+      </TableCell>
+
+      <TableCell>
+        <Button
+          onDoubleClick={() => removePizzaFn({ pizzaId: pizza.pizzaId })}
+          disabled={isRemovingPizza}
+          variant="outline"
+          size="xs"
+        >
+          {isRemovingPizza && (
+            <Loader2 className="mr-2 h-3 w-3 animate-spin" />
+          )}
+          <Trash className="h-3 w-3" />
+          <span className="sr-only">Deletar pizza</span>
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
---
+++ src/pages/app/pizzas/pizzas-table-skeleton.tsx
@@ -0,0 +1,63 @@
+import { Search, Trash } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function PizzasTableSkeleton() {
+  return (
+    <>
+      {Array.from({ length: 10 }).map((_, i) => {
+        return (
+          <TableRow key={i}>
+            <TableCell>
+              <Button variant="outline" size="xs" disabled>
+                <Search className="h-3 w-3" />
+                <span className="sr-only">Detalhes da pizza</span>
+              </Button>
+            </TableCell>
+
+            <TableCell className="font-mono text-xs font-medium">
+              <Skeleton className="h-4 w-[160px]" />
+            </TableCell>
+
+            <TableCell className="text-muted-foreground">
+              <Skeleton className="h-4 w-[120px]" />
+            </TableCell>
+
+            <TableCell>
+              <Skeleton className="h-4 w-auto" />
+            </TableCell>
+
+            <TableCell className="font-medium">
+              <Skeleton className="h-4 w-[60px]" />
+            </TableCell>
+
+            <TableCell>
+              <Skeleton className="h-4 w-[60px]" />
+            </TableCell>
+
+            <TableCell>
+              <Skeleton className="h-4 w-[70px]" />
+            </TableCell>
+
+            <TableCell>
+              <Skeleton className="h-4 w-[75px]" />
+            </TableCell>
+
+            <TableCell>
+              <Skeleton className="h-4 w-[100px]" />
+            </TableCell>
+
+            <TableCell>
+              <Button variant="outline" size="xs" disabled>
+                <Trash className="h-3 w-3" />
+                <span className="sr-only">Deletar pizza</span>
+              </Button>
+            </TableCell>
+          </TableRow>
+        )
+      })}
+    </>
+  )
+}
